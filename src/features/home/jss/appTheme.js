6b9d5bc6e942db184c8aeb75bf0bccb5865199cc
Ceazor <Ceahorse@gmail.com>
2022-06-28 10:41:55 +0800

    color

--- src/features/home/jss/appTheme.js
+++ src/features/home/jss/appTheme.js
@@ -5,48 +5,48 @@ const createThemeMode = isNightMode =>
     palette: {
       type: isNightMode ? 'dark' : 'light',
       background: {
-        default: isNightMode ? '#242332' : '#FBE9C5',
-        paper: isNightMode ? '#606077' : '#F4E2C2',
-        primary: isNightMode ? '#505067' : '#F4E2C2',
-        secondary: isNightMode ? '#3B3A4D' : '#F4E2C2',
-        extra: isNightMode ? '#242332' : '#F4E2C2',
+        default: isNightMode ? '#242332' : '#FDE7C2',
+        paper: isNightMode ? '#606077' : '#F4D7AB',
+        primary: isNightMode ? '#505067' : '#F4D7AB',
+        secondary: isNightMode ? '#3B3A4D' : '#F4D7AB',
+        extra: isNightMode ? '#242332' : '#F4D7AB',
         dark: isNightMode ? '#2B2A3D' : '#999',
         paused: isNightMode ? '#2B2A5A' : '#FCE57E',
         retired: isNightMode ? '#d32f2f' : '#e57373',
         hover: isNightMode ? '#2B2A3D' : '#EFE6DC',
         border: isNightMode ? '#2B2A3D' : '#DED9D5',
-        overlay: isNightMode ? '#F4E2C2' : 'rgba(255, 255, 255, 0.75)',
+        overlay: isNightMode ? '#F4D7AB' : 'rgba(255, 255, 255, 0.75)',
       },
       primary: {
-        main: isNightMode ? '#F4E2C2' : '#000',
+        main: isNightMode ? '#F4D7AB' : '#000',
       },
       secondary: {
-        main: isNightMode ? '#F4E2C2' : '#F4E2C2',
+        main: isNightMode ? '#F4D7AB' : '#F4D7AB',
       },
       text: {
-        primary: isNightMode ? '#F4E2C2' : '#000',
+        primary: isNightMode ? '#F4D7AB' : '#000',
         secondary: isNightMode ? '#B0B0DD' : '#00000066',
-        flipped: isNightMode ? '#000' : '#F4E2C2',
+        flipped: isNightMode ? '#000' : '#F4D7AB',
       },
     },
     overrides: {
       MuiButton: {
         label: {
-          color: isNightMode ? '#F4E2C2' : '#000',
+          color: isNightMode ? '#F4D7AB' : '#000',
         },
       },
       // for dropdown menu items
       MuiButtonBase: {
         root: {
-          color: isNightMode ? '#F4E2C2' : '#000',
+          color: isNightMode ? '#F4D7AB' : '#000',
         },
       },
       MuiCheckbox: {
         colorPrimary: {
-          color: isNightMode ? '#F4E2C2' : '#000',
+          color: isNightMode ? '#F4D7AB' : '#000',
         },
         colorSecondary: {
-          color: isNightMode ? '#F4E2C2' : '#000',
+          color: isNightMode ? '#F4D7AB' : '#000',
         },
       },
     },
